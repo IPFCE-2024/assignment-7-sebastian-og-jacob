2ecf36d17d4bc73a0294dc9bc1482d2b9028ce9e
AnarchistAristocrat <sebkittel123@gmail.com>
2024-10-10 13:41:50 +0200

    Lavede 1a

--- taylor_sine.c
+++ taylor_sine.c
@@ -1,6 +1,28 @@
 #include "taylor_sine.h"
+#include <assert.h>
+#include <stdio.h>
+#define PI 3.14159265358979
 
 double taylor_sine(double x, int n) {
-  // implement your function here
-  return -1.0;
+  assert(n > 0);
+    int div = x / (2 * PI);
+    double num = x - (double)div * 2 * PI;
+    double part = num;
+    double output = part; 
+    for(int i = 1; i < n; i++){
+        part *= -1;
+        part = part*num;
+        part = part/(i*2+1);
+        part = part*num;
+        part = part/(i*2);
+        output += part;
+    }
+    return output;
 }
+
+/*
+int main(){
+  printf("%lf", taylor_sine(PI/4, 11));
+  return 0;
+}
+*/
