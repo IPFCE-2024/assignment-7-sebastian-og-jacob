2388e8b0096dade9fb6961a41a42fb16697a4ac4
GamerJacob <jacob.steinthal@gmail.com>
2024-10-10 14:51:54 +0200

    Tilføjelse

--- main_taylor_sine.c
+++ main_taylor_sine.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main(void){
-    // Sygt episk kode
+    // Sygt episk kode - Ja det er det
     int num;
     scanf("%l",&num);
     printf("Vores version %lf math version %lf",taylor_sine(num,100), sin(num));
