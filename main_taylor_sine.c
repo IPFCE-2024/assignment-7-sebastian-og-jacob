2c8f7c695f433984b01d4fb83b46ad4455954770
AnarchistAristocrat <sebkittel123@gmail.com>
2024-10-10 15:04:46 +0200

    Fiksede test

--- main_taylor_sine.c
+++ main_taylor_sine.c
@@ -4,8 +4,8 @@
 
 int main(void){
     // Sygt episk kode - Ja det er det
-    int num;
-    scanf("%l",&num);
+    double num;
+    scanf("%lf",&num);
     printf("Vores version %lf math version %lf",taylor_sine(num,100), sin(num));
 
     return 0;
